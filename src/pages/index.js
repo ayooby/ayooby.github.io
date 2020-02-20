dd695a64fcdae37a590376f57171156bb0446b5d
Sajad Ayooby <sajad666666@gmail.com>
2020-02-20 22:57:22 +0100

    using netlify and gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to my Personal Blog</p>
+    <p>This Blog is not ready yet! sorry.</p>
+  </Layout>
+)
+
+export default IndexPage
